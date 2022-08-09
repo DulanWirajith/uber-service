f51c30511fbea8df977f450e8a54e2e08ccfa7af
dulanwirajith <dulanwirajith1995@gmail.com>
2022-08-09 14:25:00 +0530

    time issue fixed

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get("/", function (req, res) {
 ioConnect.Realtimeupdate();
 ioconnect2.setAvailability();
 
-http.listen(4040, function () {
+http.listen(4000, function () {
   console.log("listen on port 4040");
 });
